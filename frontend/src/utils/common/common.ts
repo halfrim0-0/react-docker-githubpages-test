6bf1211c422ef529ff39d73272299c43af74dde2
halfrim0-0 <hitonihiki.develop@gmail.com>
2023-04-10 15:18:21 +0900

    tailwind の記述方法を修正

--- frontend/src/utils/common/common.ts
+++
@@ -1,1 +0,0 @@
-export const themeColor: string = "blue";
--- frontend/src/utils/common/footer.ts
+++ frontend/src/utils/common/footer.ts
@@ -1,7 +1,5 @@
-import { themeColor } from "./common";
-
 export const menuWidth = "w-1/2";
 export const menuHeight = "py-5";
-export const menuBorder = `border border-${themeColor}-100`;
+export const menuBorder = `border border-blue-100`;
 export const menuTextColor = "text-white";
-export const menuBackgroundColor = `bg-${themeColor}-400 hover:bg-${themeColor}-300`;
+export const menuBackgroundColor = `bg-blue-400 hover:bg-blue-300`;
