828b550695cfc7328745aa68915108031bbabc6b
halfrim0-0 <hitonihiki.develop@gmail.com>
2023-04-10 15:05:49 +0900

    Footer のデザイン調整

--- frontend/src/utils/common/footer.ts
+++ frontend/src/utils/common/footer.ts
@@ -4,4 +4,4 @@ export const menuWidth = "w-1/2";
 export const menuHeight = "py-5";
 export const menuBorder = `border border-${themeColor}-100`;
 export const menuTextColor = "text-white";
-export const menuBackgroundColor = `bg-${themeColor}-400`;
+export const menuBackgroundColor = `bg-${themeColor}-400 hover:bg-${themeColor}-300`;
