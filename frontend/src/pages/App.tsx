d93675289e853c85db3ab8f821ea9cc7496f66a9
halfrim0-0 <hitonihiki.develop@gmail.com>
2023-04-10 14:37:15 +0900

    Page1 の追加

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/common/Footer";
+import Page1 from "./Page1";
+
+function App() {
+  return (
+    <div>
+      <Page1 />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Page1.tsx
@@ -0,0 +1,9 @@
+function Page1() {
+  return (
+    <div>
+      <h1>ここは Page1 です</h1>
+    </div>
+  );
+}
+
+export default Page1;
